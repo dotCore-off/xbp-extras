ccbe5752517b73b20ca6d6bac02aa0cc21e348d0
dotCore <64563384+Real-dotCore@users.noreply.github.com>
2020-11-04 09:16:12 +0100

    Created Be in a team challenge
    Created this challenge and change it so anyone can edit it easier.

---
+++ lua/battlepass/challenges/be_a_team.lua
@@ -0,0 +1,18 @@
+local CHALLENGE = BATTLEPASS:CreateTemplateChallenge()
+
+-- Change these to whatever you want
+CHALLENGE:SetName("Be the XXXXX")
+CHALLENGE:SetIcon("battlepass/challenges/icon.png")
+CHALLENGE:SetProgressDesc("Be a XXXXX :goal more rounds")
+CHALLENGE:SetFinishedDesc("Was a XXXXX :goal times")
+CHALLENGE:SetID("be_a_team")
+
+-- You may change this hook depending of your gamemode
+-- You have to take the hook where round is started and teams are assigned
+CHALLENGE:AddHook("OnRoundStart", function(self, ply)
+  if (ply:Team() == TEAM_XXXX) then
+    self:AddProgress(1)
+    self:NetworkProgress()
+  end
+end)
+BATTLEPASS:RegisterChallenge(CHALLENGE)

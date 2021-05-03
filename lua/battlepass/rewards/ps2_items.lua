dfbcea20fba187078200b5685a231d0e0a42a877
dotCore <64563384+dotCore-off@users.noreply.github.com>
2021-05-03 15:47:52 +0200

    Update ps2_items.lua
    Improved comments (as I'll use it for templates) + Added configurable description field

--- lua/battlepass/rewards/ps2_items.lua
+++ lua/battlepass/rewards/ps2_items.lua
@@ -1,38 +1,53 @@
--- Do not delete
+// Do not touch
 local REWARD = BATTLEPASS:CreateReward()
 
--- Unlock if tier is reached
+// Verification function
 function REWARD:CanUnlock(ply, reward)
+	// Unlock the item
+	// You may actually put a CustomCheck here if you want
 	return true
 end
 
--- Message that is shown to user when reward is hovered
+// Tooltip function
 function REWARD:GetTooltip(reward)
-	return "Unlock this permanently !"
+	// Get the reward
+	local wep = ps2_items.Get(reward)
+
+	// Return configured Description or a static text
+	return ps2_items.Description or "Unlock this !"
 end
 
--- Get and show model function on reward
+// Model function
 function REWARD:GetModel(reward)
+	// Get the reward
 	local wep = ps2_items.Get(reward)
 	if (!wep) then return ".mdl" end
-
+	
+	// Return configured Model
 	return ps2_items.Model or ".mdl"
 end
 
--- Get and show item name on reward
+// Name function
 function REWARD:GetName(reward) 
+	// Get the reward
 	local wep = ps2_items.Get(reward)
+
+	// Return configured Print Name or a static text
 	return wep and wep.PrintName or "Unknown skin"
 end
 
--- Function that is run on unlock
+// On unlock, do ...
 function REWARD:Unlock(ply, reward, amount)
+	// SERVER only
 	if (CLIENT) then return end
-    -- Item var
+
+    // Item variable
+	// Read Pointshop 2 documentation for more informations
     local itemClass = Pointshop2.GetItemClassByPrintName(reward)
-    -- Command to give a PS2 item
+
+    // Add the above item to player's inventory
 	return ply:PS2_EasyAddItem(itemClass.className)
 end
 
--- Reward registration
+// Reward registration
 REWARD:Register("ps2_items")

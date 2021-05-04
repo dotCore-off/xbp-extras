d45ddb0eb6af14b93048bdf4fa63e52e72f9742b
dotCore <64563384+Real-dotCore@users.noreply.github.com>
2021-05-05 01:20:39 +0200

    Update sh_ps_items.lua
    Fixed an issue with Tooltip's registration + Added comments

--- lua/battlepass/rewards/sh_ps_items.lua
+++ lua/battlepass/rewards/sh_ps_items.lua
@@ -10,30 +10,29 @@ end
 
 // Tooltip function
 function REWARD:GetTooltip(reward)
-	// Get the reward
-	local wep = sh_ps_items.Get(reward)
-
-	// Return configured Description or a static text
-	return sh_ps_items.Description or "Unlock this !"
+	// Return a static string
+	// You can use a custom thingy here
+	return "Unlock this permanently !"
 end
 
-// Model function
+// Icon function
 function REWARD:GetModel(reward)
 	// Get the reward
-	local wep = sh_ps_items.Get(reward)
+	local wep = ps2_items.Get(reward)
+	// Small verif
 	if (!wep) then return ".mdl" end
-	
-	// Return configured Model
-	return sh_ps_items.Model or ".mdl"
+
+	// Return the reward's model to display
+	return ps2_items.Model or ".mdl"
 end
 
 // Name function
 function REWARD:GetName(reward) 
 	// Get the reward
-	local wep = sh_ps_items.Get(reward)
+	local wep = ps2_items.Get(reward)
 
-	// Return configured Print Name or a static text
-	return wep and wep.PrintName or "Unknown skin"
+	// Return the reward's name or a static string
+	return wep and wep.PrintName or "Unknown item"
 end
 
 // On unlock, do ...
@@ -41,10 +40,10 @@ function REWARD:Unlock(ply, reward, amount)
 	// SERVER only
 	if (CLIENT) then return end
 
-    // Item variable
+	// Get the item stored in reward
 	local itm = self:NewItemTable(reward)
 
-	// Add the item to player's inventory
+	// Add it to player's inventory
 	return ply:SH_AddItem(itm, true, true)
 end
 

07c3ea0ea7252b0b00119ef34174fe82fe948e29
dotCore <64563384+Real-dotCore@users.noreply.github.com>
2021-05-05 02:05:29 +0200

    Updated vars

--- lua/battlepass/challenges/long_range.lua
+++ lua/battlepass/challenges/long_range.lua
@@ -9,7 +9,7 @@ CHALLENGE:SetFinishedDesc("Killed :goal players on a long distance")
 CHALLENGE:SetID("long_range")
 
 // Exactly the same as close_range.lua, we hook on a Player's death
-CHALLENGE:AddHook("PlayerDeath", function(self, victim, inflictor, attacker)
+CHALLENGE:AddHook("PlayerDeath", function(self, ply, victim, inflictor, attacker)
     // Avoid progress through suicides
     // Handle life's crualty ma dude
     if (victim == attacker) then return end
@@ -23,7 +23,7 @@ CHALLENGE:AddHook("PlayerDeath", function(self, victim, inflictor, attacker)
     // We first make sure the Player is the Attacker
     // Then, we look if the Victim is another Player
     // Finally, distance check
-    if (self:IsValid() and self == attacker and victim:IsPlayer() and attacker:GetPos():Distance(victim:GetPos()) > longDistance) then
+    if (ply:IsValid() and ply == attacker and victim:IsPlayer() and attacker:GetPos():Distance(victim:GetPos()) > longDistance) then
         self:AddProgress(1)
         self:NetworkProgress()
     end
